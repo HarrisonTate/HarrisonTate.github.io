a86cd47c4b2b584ab47defd8cc3bc561e6680df2
OJPARKINSON <olliepar01@gmail.com>
2018-08-10 17:35:53 +0100

    multi
    link update so there is a contact email. added a footer and reduced whitespace on js documents

--- Resources/config.js
+++ Resources/config.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
     var userFeed = new Instafeed({
         get: 'user',
         userId: '1245984943',
@@ -10,12 +8,7 @@ $(document).ready(function() {
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
-
-
     userFeed.run();
-
-
-    // This will create a single gallery from all elements that have class "gallery-item"
     $('.gallery').magnificPopup({
         type: 'image',
         delegate: 'a',
