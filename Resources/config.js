d3cff90607ed11e636576b3e4b759d3b5efbfa11
OJPARKINSON <olliepar01@gmail.com>
2018-08-10 14:44:23 +0100

    blank

---
+++ Resources/config.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '1245984943',
+        limit: 100,
+        resolution: 'standard_resolution',
+        accessToken: '1245984943.1677ed0.2763e23906a2422980a1ede31a318b7c',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+});
