d3d6d1a8f22946f440d556b2eee14bafaf2bafae
OJPARKINSON <olliepar01@gmail.com>
2019-01-03 15:07:40 +0000

    navbar fixed

--- Resources/HT.js
+++ Resources/HT.js
@@ -1,23 +1,8 @@
 function openInstaFrame(i) {
     document.getElementById("instaFrame").style.display = "block";
-    var eachImg = ["BjFQmnjnFh_", "BjFQqXDHEIb", "BjFRERWHs3v", "BjHeSKwnP0x",  "Bh_ZcF_neiE", "BgTM2MpBrB5"];
-    var imgSetter = "https://instagram.com/p/" + eachImg[i] + "/embed"
+    var imgSetter = "https://instagram.com/p/" + i + "/embed"
     document.getElementById("iframeCenter").src = imgSetter;
 } 
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-}
 function openNav() {
     document.getElementById("myNav").style.display = "block";
 }  
